39387fa871b8d7623cbf147c7aa704ae62c537de
Datorium <hello@datorium.lv>
2021-01-28 17:45:24 +0200

    Game Zone added

---
+++ Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SnakeGameWinforms
+{
+    public partial class Game : Form
+    {
+        GameZone gameZone = null;
+        
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            //adding game zone to the game
+            gameZone = new GameZone { 
+                Left = 40,
+                Top = 40
+            };
+            this.Controls.Add(gameZone);
+
+            //adding snake to the game
+
+
+            //adding food to the game
+
+
+        }
+    }
+}
---
+++ GameZone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SnakeGameWinforms
+{
+    class GameZone : PictureBox
+    {
+        public GameZone()
+        {
+            InitializeGameZone();
+        }
+
+        private void InitializeGameZone()
+        {
+            this.BackColor = Color.SteelBlue;
+            this.Width = 400;
+            this.Height = 400;
+        }
+    }
+}
